986c7a46d86f65ea4dd308c8f93f6cfb45be0436
Dowan Kim <forty4@gmail.com>
2018-09-19 22:19:13 +0900

    Static Image

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { StyleSheet, View } from 'react-native';
 
 import PlaceInput from './src/components/PlaceInput/PlaceInput';
 import PlaceList from './src/components/PlaceList/PlaceList';
+import placeImage from './src/assets/beautiful-place.jpg';
 
 export default class App extends React.Component {
   state = {
@@ -19,7 +20,8 @@ export default class App extends React.Component {
       return {
         places: prevState.places.concat({
           key: Math.random(),
-          value: placeName,
+          name: placeName,
+          image: placeImage,
         })
       };
     });
